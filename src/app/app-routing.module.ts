2a63f37b84806118eb4d7b5fb56278a5db74adf8
Joseph Rios Henao <joseph232005@gmail.com>
2021-06-05 20:06:38 -0500

    contine late with landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
+  {path:'', component: LandingPageComponent},
 ];
 
 @NgModule({
