c9fc4937e74eecc83284d1cc9c3c917431daed69
Joseph Rios Henao <joseph232005@gmail.com>
2021-07-31 15:13:18 -0500

    idea for profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  user:any = {
+    name: 'Joseph Rios Henao',
+    email: 'joseph232005@gmail.con',
+    tel: '+55 (11) 99876-8787',
+    address: 'Rua do Cemitério, 645',
+    image: 'https://avatars3.githubusercontent.com/u/147914?v=3&s=460',
+    emailVerified: true,
+    location: 'Brazil, Rio de Janeiro'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
