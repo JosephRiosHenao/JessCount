8282389d1a9b806dffaa566dd8ec24496364a231
Joseph Rios Henao <joseph232005@gmail.com>
2021-08-06 16:05:54 -0500

    fix errors and sync data... continue with posiblies errors and upgrade data

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/services/account.service';
+import firebase from 'firebase';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,17 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  user:any = {
-    name: 'Joseph Rios Henao',
-    email: 'joseph232005@gmail.con',
-    tel: '+55 (11) 99876-8787',
-    address: 'Rua do Cemitério, 645',
-    image: 'https://avatars3.githubusercontent.com/u/147914?v=3&s=460',
-    emailVerified: true,
-    location: 'Brazil, Rio de Janeiro'
-  }
+  user!: firebase.User;
+    // name: 'Joseph Rios Henao',
+    // email: 'joseph232005@gmail.con',
+    // tel: '+55 (11) 99876-8787',
+    // address: 'Rua do Cemitério, 645',
+    // image: 'https://avatars3.githubusercontent.com/u/147914?v=3&s=460',
+    // emailVerified: true,
+    // location: 'Brazil, Rio de Janeiro'
 
-  constructor() { }
+
+  constructor( private accountService:AccountService) {
+    accountService.getUser$().subscribe((user) => {
+      this.user = user;
+      user.phoneNumber = user.phoneNumber != null ? user.phoneNumber : 'No seha registrado ningún número de teléfono';
+    })
+  }
 
   ngOnInit(): void {
   }
