aee065c3097f52522679108f998e183310f299b2
Joseph Rios Henao <joseph232005@gmail.com>
2021-08-12 15:05:18 -0500

    creating form and validators, continue with integrate visual info

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, createPlatform, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from 'src/app/services/account.service';
 
 @Component({
@@ -9,9 +10,20 @@ import { AccountService } from 'src/app/services/account.service';
 export class EditProfileComponent implements OnInit {
 
   verified:boolean = false
+  
+  editProfileForm: FormGroup = new FormGroup({});
 
-  constructor( public accountService:AccountService) { }
+  createForm(){
+    this.editProfileForm = this.formBuilder.group({
+      name: [this.accountService.user?.displayName,Validators.required],
+      tel: [this.accountService.user?.phoneNumber,Validators.required],
+    })
+  }
 
+  constructor( public accountService:AccountService, private formBuilder:FormBuilder) {
+    this.createForm()
+  }
+  
   ngOnInit(): void {
     this.verified = this.accountService.user?.emailVerified!;
   }
@@ -22,4 +34,8 @@ export class EditProfileComponent implements OnInit {
     this.accountService.confirmEmail();
   }
 
+
+  onSubmitEditProfile(){
+
+  }
 }
