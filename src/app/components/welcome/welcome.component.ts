029c64bb37268624e83dfba1700a07f4fe460282
Joseph Rios Henao <joseph232005@gmail.com>
2021-09-08 12:56:10 -0500

    add form template welcome

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,10 +9,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  user:User;
 
-  constructor() { }
+  partFinish:boolean = false;
+
+  constructor(private changeDetector:ChangeDetectorRef, private accountService:AccountService) { 
+    this.user = this.accountService.user;
+  }
 
   ngOnInit(): void {
+    this.partFinish = false;
+    this.user = this.accountService.user;
+    this.accountService.getUser$().subscribe((user) => {
+      this.user = user;
+      this.changeDetector.detectChanges();
+    })
+  }
+
+  changePhotoURL(e:Event){
+    this.changeDetector.detectChanges();
+    if (e.target != null){
+      let file : File = (e.target as HTMLInputElement).files?.item(0)!;
+      this.accountService.updatePhoto(file)
+    }
   }
 
 }
